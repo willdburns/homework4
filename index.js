bb7412e31e879dce2f4e43352cb5395804c63221
Will Burns <will.d.burns@gmail.com>
2019-08-10 13:40:00 -0500

    initial upload
    ghgjhvjh

---
+++ index.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal({ reset: true });
+
+
+sr.reveal('#bannerinfo', { 
+  duration: 2000 
+});
+
+sr.reveal('#cartoon', { 
+  duration: 2000 
+});
+
+sr.reveal('.service-img', { 
+  origin: 'right', 
+  duration: 4000 
+});
+
+sr.reveal('.fade1', { 
+  origin: 'right', 
+  duration: 4000 
+});
+
+/*sr.reveal('.fade2', { 
+  origin: 'right', 
+  duration: 4000 
+});*/
+
+sr.reveal('.fade2', { 
+  rotate: { x: 100, y: 100, z: 0 },
+  duration: 1000
+});
+
+  
